3fd14546f4de443a2ec577e0c536d686296d5b4d
Rakshak <rakshakgaind02@gmail.com>
2022-11-04 18:47:25 +0530

    Initial Commit

---
+++ app/src/main/java/com/project/news/db/ArticleDao.kt
@@ -0,0 +1,29 @@
+package com.project.news.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.project.news.models.Article
+
+/*
+* Database Access operation (DAO) defined here, with Room Library
+* */
+@Dao
+interface ArticleDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
+
+    @Query("select * from articles")
+    fun getArticles(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+
+    @Query("select count() from articles where url=:url")
+    suspend fun getCount(url: String): Long
+
+}
